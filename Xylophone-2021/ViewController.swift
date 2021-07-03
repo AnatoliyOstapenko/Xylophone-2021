6425a03fcfd7d7cec2a11d0edc3c0e4d1a0be9a3
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-07-03 13:03:36 +0300

    Buttons, URL session, Player added
    7 Buttons added, URL session for only one button created.
    Player created, one sound run successfully.

--- Xylophone-2021/ViewController.swift
+++ Xylophone-2021/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+// 1) Import AVFoundation
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
-
+    // 2) Create variable audioPlayer into the class only
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func keyButtonPressed(_ sender: UIButton) {
+        //create URL session
+        let url = Bundle.main.url(forResource: "A", withExtension: "wav")
+        
+        // url became optiona, so I have to unwrap url, I use guard (it's shortage way)
+        guard url != nil else {
+            return
+        }
+        // Create the audio player and play the a sound from url session
+        // transfer to variable URL session, add try, and add do - carch block
+        // also I have to add exclamation mark to url
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+            
+        } catch {
+            print("erroe popped up, let's RFM again")
+        }
+        
+        
+    }
 }
+
+
+
+    
+
 
