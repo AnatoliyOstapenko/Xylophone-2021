bd9f7afa929403bb598b4e5d635e0e4b0b70f261
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-07-03 16:14:57 +0300

    Update ViewController.swift
    create func playSound - this step is not needed, it's just to practice more function skil

--- Xylophone-2021/ViewController.swift
+++ Xylophone-2021/ViewController.swift
@@ -21,21 +21,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func anyButtonPressed(_ sender: UIButton) {
+        
         // 6) create constant pressedButton (becuase never mutaded in the further line)
-        // 6) transfer current title of button pressed to pressedButton
-        let pressedButton = sender.currentTitle
+        // transfer current title of button pressed to pressedButton
         
-        // 3) create URL session
-        // 7) change a name tittle to pressedButton
-        let url = Bundle.main.url(forResource: pressedButton, withExtension: "wav")
+        if let pressedButton = sender.currentTitle {
+            playSound(button: pressedButton)
+        } else {
+            print("Another error")
+        }
         
-        // 4) url became optiona, so I have to unwrap url, I use guard (it's shortage way)
+        
+    }
+    // 3) create func playSound - this step is not needed, it's just to practice more function skill
+    // 4) create input button to interact with anyButtonPressed function
+    
+    func playSound(button: String) {
+        
+        // 5) create URL session
+        // 6) change a name tittle to pressedButton
+        let url = Bundle.main.url(forResource: button, withExtension: "wav")
+        
+        // 7) url became optiona, so I have to unwrap url, I use guard (it's shortage way)
         guard url != nil else {
             return
         }
-        // 5) Create the audio player and play the a sound from url session
-        // 5) transfer to variable URL session, add try, and add do - carch block
-        // 5) also I have to add exclamation mark to url
+        // 8) Create the audio player and play the a sound from url session
+        // transfer to variable URL session, add try, and add do - carch block
+        // also I have to add exclamation mark to url
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url!)
             audioPlayer?.play()
@@ -43,8 +56,6 @@ class ViewController: UIViewController {
         } catch {
             print("error popped up, let's RFM again")
         }
-        
-        
     }
 }
 
